bb23ac5c078adc49ca46b61a940c4e60c109e593
shinjans <shinjan.s@gmail.com>
2020-04-13 20:16:14 -0500

    New template for basic UI5 app

---
+++ generators/basic/index.js
@@ -0,0 +1,60 @@
+
+var Generator = require("yeoman-generator");
+const glob = require("glob");
+
+module.exports = class extends Generator {
+
+  // The name `constructor` is important here
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+    //this.argument("appname", { type: String, required: true });
+  }
+
+  async prompting() {
+    const answers = await this.prompt([{
+      type: "input",
+      name: "projectname",
+      message: "Your project name",
+      default: this.appname
+    },
+    {
+      type: "input",
+      name: "namespace",
+      message: "Namespace (In reverse DNS)",
+      default: "com.mycompany.module.www"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Description"
+    }
+    ]).then((answers) => {
+      this.destinationRoot(`${answers.projectname}`);
+      this.config.set(answers);
+    });
+  }
+
+  writing() {
+    this.options.oneTimeConfig = this.config.getAll();
+    this.options.oneTimeConfig.fullNamespace = this.options.oneTimeConfig.namespace + "." + this.options.oneTimeConfig.projectname;
+    this.options.oneTimeConfig.namespacePath = this.options.oneTimeConfig.namespace + "/" + this.options.oneTimeConfig.projectname;
+
+    glob.sync("**", {
+      cwd: this.sourceRoot(),
+      nodir: true
+    }).forEach((file) => {
+      const sOrigin = this.templatePath(file);
+      const sTarget = this.destinationPath(file.replace(/^_/, "").replace(/\/_/, "/"));
+      this.fs.copyTpl(sOrigin, sTarget, this.options.oneTimeConfig);
+    });
+
+  }
+
+  install() {
+    // this.installDependencies({
+    //   bower: false,
+    //   npm: false
+    // });
+  }
+};
---
+++ generators/basic/templates/webapp/controller/Main.controller.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	"./BaseController",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+	"use strict";
+
+	return BaseController.extend("<%= fullNamespace  %>.controller.Main", {
+		oUIModel: null,
+		formatter: formatter,
+
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
+
+		/**
+		 * Called when the worklist controller is instantiated.
+		 * @public
+		 */
+		onInit: function () {
+			this.oUIModel = this.getOwnerComponent().getUIModel();
+		},
+
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
+
+		/**
+		 * Event handler for navigating back.
+		 * We navigate back in the browser history
+		 * @public
+		 */
+		onNavBack: function () {
+			history.go(-1);
+		},
+
+		onMessagePopoverPress: function (oEvent) {
+			this._getMessagePopover().openBy(oEvent.getSource());
+		},
+
+
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
+
+
+
+	});
+});
